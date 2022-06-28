27520d0b96d23ff3bc603934b83582ca59f44bed
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2022-06-28 20:37:26 +0530

    Added textFom.js Learning about state

--- src/components/textForm.js
+++ src/components/textForm.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function TextForm(props) {
   return (
     <div>
-        <h1>{props.heading}</h1>
-      <label for="myBox" className="form-label">{props.text}</label>
-  <textarea className="form-control my-3" id="myBox" rows="8"></textarea>
+      <h1>{props.heading}</h1>
+      <textarea className="form-control my-3" id="myBox" rows="8"></textarea>
+      <button type="button" class="btn btn-primary">Convert to UPPERCASE</button>
     </div>
   );
 }
