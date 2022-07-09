e6077a503eae0f405cdce00f191dd0efdeeb06c8
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2022-07-09 13:26:37 +0530

    About removed where we wer e using react router DOM

--- src/App.js
+++ src/App.js
@@ -6,50 +6,50 @@ import Navbar from "./components/Navbar.js";
 import TextForm from "./components/textForm.js";
 // import About from "./components/About";
 import Alert from "./components/Alert.js";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode,setMode] = useState("light");
-  const [alert,setAlert] = useState(null);
-  const [blindMode,setBlindMode] = useState("blindOff");
-  
-  
-  const showAlert=(message,type)=>{
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const [blindMode, setBlindMode] = useState("blindOff");
+
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
-    })
+      msg: message,
+      type: type,
+    });
     setTimeout(() => {
       setAlert(null);
     }, 1500);
-  }
+  };
 
   const toogleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "black";
-      showAlert("Dark Mode has been Enabled", "success")
-    } else{
+      showAlert("Dark Mode has been Enabled", "success");
+    } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
-      showAlert("Light Mode has been Enabled", "success")
+      showAlert("Light Mode has been Enabled", "success");
     }
   };
 
   const toogleBlindMode = () => {
-    if(blindMode==="blindOff"){
+    if (blindMode === "blindOff") {
       setBlindMode("blindOn");
       document.body.style.backgroundColor = "#3330E4";
-      showAlert("Blind Mode has been Enabled", "success")
-      console.log("Blind Modeee on")
-    }else if(blindMode==="blindOn"){
+      showAlert("Blind Mode has been Enabled", "success");
+      console.log("Blind Modeee on");
+    } else if (blindMode === "blindOn") {
       setBlindMode("blindOff");
       setMode("light");
       document.body.style.backgroundColor = "white";
-      showAlert("Blind Mode Disabled","danger")
-      console.log("Blind Modeee off")
+      showAlert("Blind Mode Disabled", "danger");
+      console.log("Blind Modeee off");
     }
-  };  
-  
+  };
+
   const capitalize = (word) => {
     const lower = word.toLowerCase();
     return lower.charAt(0).toUpperCase() + lower.slice(1);
@@ -57,54 +57,53 @@ function App() {
 
   return (
     <>
-      <Navbar
-        title="Textify"
-        aboutText="About"
-        mode={mode}
-        blindMode={blindMode}
-        capitalize={capitalize}
-        toogleMode={toogleMode}
-        toogleBlindMode={toogleBlindMode}
-      />
-      <Alert alert={alert} capitalize={capitalize}/>
-      <div className="container">
-        <TextForm
-          text="here is your text"
-          heading="Enter the Text to analyse"
-          textSummary="Your Text Summary"
+      {/* <Router> */}
+        <Navbar
+          title="Textify"
+          aboutText="About"
           mode={mode}
           blindMode={blindMode}
           capitalize={capitalize}
-          alert={alert}
-          showAlert={showAlert}
+          toogleMode={toogleMode}
           toogleBlindMode={toogleBlindMode}
         />
-        {/* <About /> */}
-      </div>
+        <Alert alert={alert} capitalize={capitalize} />
+        <div className="container">
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />} /> */}
+            {/* <Route
+              exact
+              path="/"
+              element={
+                <TextForm
+                  text="here is your text"
+                  heading="Enter the Text to analyse"
+                  textSummary="Your Text Summary"
+                  mode={mode}
+                  blindMode={blindMode}
+                  capitalize={capitalize}
+                  alert={alert}
+                  showAlert={showAlert}
+                  toogleBlindMode={toogleBlindMode}
+                />
+              }
+            />
+          </Routes> */}
+          <TextForm
+                  text="here is your text"
+                  heading="Enter the Text to analyse"
+                  textSummary="Your Text Summary"
+                  mode={mode}
+                  blindMode={blindMode}
+                  capitalize={capitalize}
+                  alert={alert}
+                  showAlert={showAlert}
+                  toogleBlindMode={toogleBlindMode}
+                />
+        </div>
+      {/* </Router> */}
     </>
   );
 }
 
 export default App;
-
-//example from docs
-// const comment = {
-//   text: 'I hope you enjoy learning React!',
-//   author: {
-//     name: 'Hello Kitty',
-//     avatarUrl: 'http://placekitten.com/g/64/64'
-//   }
-// };
-
-// function App() {
-//   return (
-//     <>
-//     <Comment
-//     text={comment.text}
-//     author={comment.author} />
-//     </>
-
-//   );
-// }
-
-// export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Proptypes from "prop-types";
+// import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -7,6 +8,9 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.blindMode==="blindOn"?"blind":props.mode==="dark"?"dark" :"light"}` }
     >
       <div className="container-fluid">
+        {/* <Link className="navbar-brand" to="/">
+          {props.title}
+        </Link> */}
         <a className="navbar-brand" href="/">
           {props.title}
         </a>
@@ -24,47 +28,21 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active clr" aria-current="page" href="/">
+              {/* <Link className="nav-link active clr" aria-current="page" to="/">
+                Home
+              </Link> */}
+              <a className="nav-link active clr" aria-current="page" from="/">
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              {/* <Link className="nav-link" to="/about">
                 {props.aboutText}
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="/"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a className="dropdown-item" href="/">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
+              </Link> */}
+              {/* <a className="nav-link" from="/about">  
+                {props.aboutText}
+              </a> */}
+            </li> 
           </ul>
 
           <div
@@ -102,7 +80,8 @@ export default function Navbar(props) {
               props.mode === "light"
                 ? "Dark"
                 : "Light"
-            } Mode`}</label>
+            } Mode`}
+            </label>
           </div>
         </div>
       </div>
